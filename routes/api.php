026f63e45e176d0ff5054f523a356c7cf2b2719a
Ivan Copeland <ivan.copeland2015@gmail.com>
2025-01-15 12:08:45 +0200

    1

--- routes/api.php
+++ routes/api.php
@@ -29,6 +29,12 @@ Route::middleware('auth:sanctum')->group(function () {
     //Route::apiResource('posts', \App\Http\Controllers\Api\PostController::class);
 
 
+    // API routes for tasks-tatuses
+    Route::apiResource('tasks-tatuses', \App\Http\Controllers\Api\TaskstatusController::class);
+    // API routes for tasks
+    Route::apiResource('tasks', \App\Http\Controllers\Api\TaskController::class);
+    // API routes for todo-lists
+    Route::apiResource('todo-lists', \App\Http\Controllers\Api\TodoListController::class);
 
 });
 
@@ -39,6 +45,18 @@ Route::middleware('auth:sanctum')->group(function () {
 //Route::get('posts/{post}', [\App\Http\Controllers\Api\PostController::class, 'show'])->name('posts.show');
 
 
-// API routes for users
-Route::get('users', [\App\Http\Controllers\Api\UserController::class, 'index'])->name('users.index');
-Route::get('users/{user}', [\App\Http\Controllers\Api\UserController::class, 'show'])->name('users.show');
+//// API routes for users
+//Route::get('users', [\App\Http\Controllers\Api\UserController::class, 'index'])->name('users.index');
+//Route::get('users/{user}', [\App\Http\Controllers\Api\UserController::class, 'show'])->name('users.show');
+
+
+
+//// API routes for tasks-tatuses
+//Route::get('tasks-tatuses', [\App\Http\Controllers\Api\TaskstatusController::class, 'index'])->name('tasks-tatuses.index');
+//Route::get('tasks-tatuses/{tasks-tatus}', [\App\Http\Controllers\Api\TaskstatusController::class, 'show'])->name('tasks-tatuses.show');
+//// API routes for tasks
+//Route::get('tasks', [\App\Http\Controllers\Api\TaskController::class, 'index'])->name('tasks.index');
+//Route::get('tasks/{task}', [\App\Http\Controllers\Api\TaskController::class, 'show'])->name('tasks.show');
+//// API routes for todo-lists
+//Route::get('todo-lists', [\App\Http\Controllers\Api\TodoListController::class, 'index'])->name('todo-lists.index');
+//Route::get('todo-lists/{todo-list}', [\App\Http\Controllers\Api\TaskController::class, 'show'])->name('todo-lists.show');
